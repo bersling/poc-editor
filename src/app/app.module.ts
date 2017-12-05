b2aa13aa14228bd4935cb48a6d7dd3bbbb2eecbe
bersling <bersling@gmail.com>
2017-12-05 18:48:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BlaComponent } from './bla/bla.component';
+import { BlubComponent } from './blub/blub.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlaComponent,
+    BlubComponent
   ],
   imports: [
     BrowserModule
